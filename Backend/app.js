c3dc7f6f1fcbb109e7466233b071cdfcf8d0d0d0
Naren Zadafiya <narenzadfiya3579@gmail.com>
2024-01-20 13:43:14 -0500

    Complete Backened Setup

--- Backend/app.js
+++ Backend/app.js
@@ -1,20 +1,5 @@
 const express = require("express");
 require("dotenv").config();
-const connectdb = require("./db/db");
 const app = express();
-var cron = require("./src/Routes/cron");
-var morgan = require("morgan");
-const indexRouter = require('./src/Routes/index')
-const adminRouter = require('./src/Admin/Routes/index')
-const sendMail = require('./src/helper/mailer')
-const fileUpload = require("express-fileupload");
-var cors = require("cors");
-async () => {
-    await connectdb()
-}
-connectdb();
-app.use(cors());
-app.use(express.json());
-app.use(morgan("dev"))
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT,()=>console.log("Server Running on port: "+process.env.PORT));
