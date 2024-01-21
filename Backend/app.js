9896342dc385c03451800500e632fd9ef6f8761f
harsh2799 <mishraharsh27@gmail.com>
2024-01-20 23:40:24 -0500

    remove code from processCSV

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
+// const ETLCSV = require("./src/crons/ETLCSV.js")
+const ExtractCSV = require("./src/crons/ExtractCSV");
 require("dotenv").config();
 const app = express();
 
+// ETLCSV()
+ExtractCSV()
 app.listen(process.env.PORT,()=>console.log("Server Running on port: "+process.env.PORT));
