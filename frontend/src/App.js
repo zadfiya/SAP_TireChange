8423f82138ef5d6140d2e73ff35e74b6fcee039c
Utsav-Virani <uvvirani004@gmail.com>
2024-01-20 21:30:11 -0500

    Sidebar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,20 @@ import { Suspense } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import { Routes } from "./Route";
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 const materialTheme = createTheme(/* your theme */);
 function App() {
   return (
-<ThemeProvider theme={materialTheme}>
-    <Router>
-      <Suspense>
-        <Routes />
-      </Suspense>
-    </Router>
+    <ThemeProvider theme={materialTheme}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Router>
+          <Suspense>
+            <Routes />
+          </Suspense>
+        </Router>
+      </LocalizationProvider>
     </ThemeProvider>
   );
 }
