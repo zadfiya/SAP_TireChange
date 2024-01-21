74bf0609e1e47c79a5a7758a6623f1967c81d78d
UIcoder511 <umang25919998@gmail.com>
2024-01-20 21:51:22 -0500

    app.js changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,22 @@ import { Suspense } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import { Routes } from "./Route";
+import DayScheduler from "./components/DayScheduler/DayScheduler";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 const materialTheme = createTheme(/* your theme */);
 function App() {
   return (
-<ThemeProvider theme={materialTheme}>
-    <Router>
-      <Suspense>
-        <Routes />
-      </Suspense>
-    </Router>
+    <ThemeProvider theme={materialTheme}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Router>
+          <Suspense>
+            <Routes />
+            {/* <DayScheduler /> */}
+          </Suspense>
+        </Router>
+      </LocalizationProvider>
     </ThemeProvider>
   );
 }
